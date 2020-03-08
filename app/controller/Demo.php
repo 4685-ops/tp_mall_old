18247374152b593a59213ba3f022faa4f52c585e
刘海洋 <liuhaiyang@gobestsoft.com>
2020-03-08 15:34:08 +0800

    第一次提交代码

---
+++ app/controller/Demo.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 757208466
+ * Date: 2020/3/8
+ * Time: 15:18
+ */
+
+namespace app\controller;
+
+use app\BaseController;
+
+class Demo extends BaseController
+{
+    public function show(){
+        $result = [
+            "status" => 1,
+            "message"=>"ok",
+            "result"=>[
+                "id"=>1
+            ]
+        ];
+
+        $header = [
+            "Token"=>'show_token'
+        ];
+
+        return json($result,500,$header);
+
+
+    }
+}
